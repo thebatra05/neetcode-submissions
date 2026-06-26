45fb6905056696df1034974b151b2a0702818918
thebatra05 <thebatra05@gmail.com>
2026-06-26 12:12:44 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int bp = prices[0];
+        int n = prices.length;
+        int ans = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (prices[i] > bp) ans = Math.max(ans, prices[i] - bp);
+            bp = Math.min(bp, prices[i]);
+        }
+
+        return ans;
+    }
+}
