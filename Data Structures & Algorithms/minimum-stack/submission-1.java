86ef2232e14054d1b837eeab594caf5655fcc4b3
thebatra05 <thebatra05@gmail.com>
2026-06-30 14:15:33 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,26 @@
+class MinStack {
+    private Stack<int[]> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if (stack.isEmpty()) stack.push(new int[]{val, val});
+        else {
+            stack.push(new int[]{val, Math.min(val, stack.peek()[1])});
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
