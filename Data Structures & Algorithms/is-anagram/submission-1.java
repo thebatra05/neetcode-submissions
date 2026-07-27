77e3360d51aecf93d8f6af98c2de358254eb554f
thebatra05 <thebatra05@gmail.com>
2026-07-26 22:53:38 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr = new int[26];
+        for (char c : s.toCharArray()) arr[c - 'a']++;
+        for (char c : t.toCharArray()) arr[c - 'a']--;
+        for (int num : arr) if (num != 0) return false;
+
+        return true; 
+    }
+}
