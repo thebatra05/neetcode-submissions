58fcd13908119a55e55f1e35f7cc6cda5c1cf49d
thebatra05 <thebatra05@gmail.com>
2026-07-28 17:55:46 -0700

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            if (s.charAt(i) == '0') dp[i] = 0;
+            else dp[i] = dp[i + 1];
+            
+            if (i < n - 1) {
+                if (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7') {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+
+        return dp[0];
+    }
+
+    private int dfs(String s, int idx, int[] memo) {
+        if (idx >= s.length()) return 1;
+        if (s.charAt(idx) == '0') return 0;
+        if (memo[idx] != -1) return memo[idx];
+        
+        int numWays = dfs(s, idx + 1, memo);
+        if (idx < s.length() - 1) {
+            if (s.charAt(idx) == '1' || s.charAt(idx) == '2' && s.charAt(idx + 1) < '7') {
+                numWays += dfs(s, idx + 2, memo);
+            }
+        }
+
+        memo[idx] = numWays;
+
+        return numWays;
+    }
+
+    /*
+        "2 2 6"
+         3 2 1 1
+
+         "0 6"
+            1  1
+    */
+
+
+}
